57416ae6cedc407bcdcab7552b3293f5e4715b90
suyash-2004 <suyash096@gmail.com>
2025-01-22 13:25:50 +0530

    fixing

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,9 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-
+import react from '@vitejs/plugin-react';
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Base path for deployment
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    port: 5173, // Set the development server port
-    open: true, // Automatically opens the browser
-  },
-  build: {
-    outDir: 'dist', // Output directory for the production build
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'), // Ensure entry point is correct
-    },
-  },
-  preview: {
-    port: 5173, // Specify a port for the preview server
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
 });
